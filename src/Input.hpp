eb8bb51a699fb67422ad43886319fe82085f7489
Cyril <brebisasthmatique@gmail.com>
2019-01-30 14:28:40 +0100

    Support manettes grace à l'implémentation d'une base de donnée

--- src/Input.hpp
+++ src/Input.hpp
@@ -3,16 +3,26 @@
 
 #include <SFML/Graphics.hpp>
 #include <vector>
+#include <map>
+#include <string>
+
+#define JOYSTICK_LEFT_X sf::Joystick::Axis::X
+#define JOYSTICK_LEFT_Y sf::Joystick::Axis::Y
 
 class Input {
 	private:
 		std::vector<int> connected_;
+		std::map<std::string, std::map<std::string, std::string>> gamePadMap_;
 	public:
 		Input();
 		void waitForConnection();
 		static Input &instance();
 		int const getConnectedPlayer();
 		static void tracePressedButton();
+		static sf::Joystick::Axis SDL2SFML(std::string axis);
+		int getAxisPosition(int player, std::string button);
+		bool isButtonPressed(int player, std::string button);
+		void parseGamePadDB(std::string name);
 };
 
 static Input inputInstance;
--- src/Utils.h
+++ src/Utils.h
@@ -2,6 +2,7 @@
 #define UTILS_H
 
 #include <vector>
+#include <string>
 
 namespace Utils
 {
@@ -11,6 +12,20 @@ namespace Utils
 			if (e == value)
 				return true;
 		return false;
+	}
+
+	std::vector<std::string> split(const std::string& str, const std::string& sep = " ")
+	{
+		std::vector<std::string> out;
+		size_t debut = 0;
+		size_t fin = str.find(sep);
+		while (debut <= str.length())
+		{
+			out.push_back(str.substr(debut, fin - debut));
+			debut = fin != std::string::npos ? fin + sep.length() : fin;
+			fin = str.find(sep, debut);
+		}
+		return out;
 	}
 }
 
